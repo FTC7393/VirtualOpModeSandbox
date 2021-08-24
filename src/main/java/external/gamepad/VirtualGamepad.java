6c4fbfc6d248a62a6a28db873539d22d0d7248a7
zbrachinara <zbrachinara@gmail.com>
2021-08-24 16:06:29 -0400

    remove broken javafx dependency

--- src/main/java/external/gamepad/VirtualGamepad.java
+++ src/main/java/external/gamepad/VirtualGamepad.java
@@ -1,8 +1,9 @@
 package external.gamepad;
 
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+//import javafx.scene.input.KeyCode;
+//import javafx.scene.input.KeyEvent;
 
+//import java.awt.event.KeyEvent;
 import java.util.Map;
 
 public class VirtualGamepad {
@@ -11,43 +12,43 @@ public class VirtualGamepad {
             dpad_up, dpad_down, dpad_left, dpad_right,
             left_stick_button, right_stick_button, back, start;
 
-    public void keyHandler(KeyEvent ev, boolean isDownEvent) {
-        // ugly switch statement
-        // see rust Entry for salvation
-        // though javalang devs are so obstinate
-        switch (ev.getCode()) {
-            case UP:
-                dpad_up = isDownEvent;
-                break;
-            case DOWN:
-                dpad_down = isDownEvent;
-                break;
-            case LEFT:
-                dpad_left = isDownEvent;
-                break;
-            case RIGHT:
-                dpad_right = isDownEvent;
-                break;
-            case W:
-                a = isDownEvent;
-                break;
-            case D:
-                b = isDownEvent;
-                break;
-            case S:
-                y = isDownEvent;
-                break;
-            case A:
-                x = isDownEvent;
-                break;
-            // now I must wallow in the fact that javafx doesn't let you tell between l/r shift or l/r ctrl
-            case Z:
-                left_bumper = isDownEvent;
-                break;
-            case M:
-                right_bumper = isDownEvent;
-                break;
-        }
-    }
+//    public void keyHandler(KeyEvent ev, boolean isDownEvent) {
+//        // ugly switch statement
+//        // see rust Entry for salvation
+//        // though javalang devs are so obstinate
+//        switch (ev.getKeyChar()) {
+//            case UP:
+//                dpad_up = isDownEvent;
+//                break;
+//            case DOWN:
+//                dpad_down = isDownEvent;
+//                break;
+//            case LEFT:
+//                dpad_left = isDownEvent;
+//                break;
+//            case RIGHT:
+//                dpad_right = isDownEvent;
+//                break;
+//            case W:
+//                a = isDownEvent;
+//                break;
+//            case D:
+//                b = isDownEvent;
+//                break;
+//            case S:
+//                y = isDownEvent;
+//                break;
+//            case A:
+//                x = isDownEvent;
+//                break;
+//            // now I must wallow in the fact that javafx doesn't let you tell between l/r shift or l/r ctrl
+//            case Z:
+//                left_bumper = isDownEvent;
+//                break;
+//            case M:
+//                right_bumper = isDownEvent;
+//                break;
+//        }
+//    }
 
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,31 +4,20 @@ import external.gamepad.VirtualGamepad;
 import external.opmode.AbstractOptionsOpMode;
 import external.opmode.HardwarelessAbstractOp;
 import external.util.OptionEntries;
-import javafx.application.Application;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main {
 
     HardwarelessAbstractOp opMode = new OptionsOp();
     Thread opThread;
     volatile boolean sigkill = false;
 
-    public void start(Stage stage) throws Exception {
+    public void start() {
 
         // CAUTION!!! This variable is referenced in opmode worker thread. It must not be nulled.
         VirtualGamepad vgp = new VirtualGamepad();
 
-        Parent root = new Label("Focus on this window to start input to gamepad");
-        stage.setTitle("win0");
-        Scene s = new Scene(root, 400, 40);
-        s.setOnKeyPressed((ev) -> vgp.keyHandler(ev, true));
-        s.setOnKeyReleased((ev) -> vgp.keyHandler(ev, false));
-
-        stage.setScene(s);
-        stage.show();
+//        s.setOnKeyPressed((ev) -> vgp.keyHandler(ev, true));
+//        s.setOnKeyReleased((ev) -> vgp.keyHandler(ev, false));
 
         // Operation runner
         opThread = new Thread(() -> {
@@ -48,14 +37,14 @@ public class Main extends Application {
 
     }
 
-    @Override
     public void stop() throws InterruptedException {
         sigkill = true;
         opThread.join();
     }
 
     public static void main(String[] args) {
-        launch(args);
+//        launch(args);
+        new Main().start();
     }
 }
 
